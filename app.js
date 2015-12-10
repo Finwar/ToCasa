090bc1c91696b22dd01e600c1873357f321f85ab
IVAN GONZALEZ MARTINEZ <ivan.gonzalezmartine@altran.es>
2015-12-10 17:58:59 +0100

    app finished

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 var utils = require('./utils');
 var request = require('request');
-var html = require('htmlparser2');
 var $ = require('cheerio');
 var async = require('async');
 var player = require('play-sound')(opts = {});
+var beep = require('beepbeep');
+var dialog = require('dialog');
 
+//TODO: Notificar Movil.
 
 var renfeUrl = 'http://horarios.renfe.com/cer/hjcer310.jsp?&nucleo=50&o=79009&d=79011&tc=DIA&td=D&df=20151208&th=1&ho=00&i=s&cp=NO&TXTInfo=';
 var myExcelJson = 'https://spreadsheets.google.com/feeds/list/1137gfrD-PQHB_0oAP2-UHGtLU3gpYFt2ak_O_I9AG2s/od6/public/basic?alt=json';
+
 
 var date = new Date();
 
@@ -26,26 +29,26 @@ async.parallel([
                     hora = "";
                 }
             }
-            callback(null, horas);
+            callback(error, horas);
         });
     },
     function (callback) {
         request(myExcelJson, function (error, response, body) {
             var array = JSON.parse(body);
-            var string = array.feed.entry[0].content.$t;//['feed']['entry'][0]['content']['$t'];
+            var string = array.feed.entry[0].content.$t; //['feed']['entry'][0]['content']['$t'];
 
             var test = string.split(",");
 
             var diaSemana = date.getDay();
             var horaSalida;
-            if (diaSemana < 4) {
+            if (diaSemana <= 4) {
                 horaSalida = test[2].substr(test[2].indexOf(':') + 2, 5);
 
 
             } else {
                 horaSalida = test[4].substr(test[4].indexOf(':') + 2, 5);
             }
-            callback(null, horaSalida);
+            callback(error, horaSalida);
 
         });
     }
@@ -53,15 +56,35 @@ async.parallel([
     var horasTren = result[0];
     var salida = result[1];
     var comprobar = 7000;
+    var media = 25;
+    console.log(horasTren);
     setInterval(function () {
-        var horaActual = utils.formatAMPM(date.getHours());
+        date = new Date();
+
+        var horaActual = date.getHours() + ":" + date.getMinutes();
 
         if (salida < horaActual) {
-            console.log(true, salida, horaActual);
-            
+            date.setMinutes(date.getMinutes() + media);
+            horaActual = date.getHours() + ":" + date.getMinutes();
+            for (var i = horasTren.length / 2; i < horasTren.length; i++) {
+                var horatren = horasTren[i].replace(".", ":");
+                if (horaActual < horatren) {
+                    if (horaActual === horatren) {
+                        console.log(true, horaActual, horatren);
+                        beep();
+                        dialog.warn('Pa casa!!!! :D');
+                    }
+
+                } else {
+                    console.log(false);
+                }
+            }
+
             //player.play('alarm.mp3', function(err){console.log(err)})
         } else {
             console.log(false);
+            console.log(salida, horaActual);
+
         }
     }, comprobar);
 
